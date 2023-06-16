4bef334497a91341c47575bd7913867e9bf90e87
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-06-16 16:18:57 +0200

    update ImageLkb.vue and 1.Tools.md

--- components/content/ImageLkb.vue
+++ components/content/ImageLkb.vue
@@ -23,14 +23,11 @@ const cssProps = computed(() => {
   const styles = {
     width: `${props.size}%`
     }
-    console.log(props.center)
   if (props.center) {
     styles.margin = 'auto'
-    console.log('if')
   }
   else{
     styles.margin = 'inherit'
-    console.log('else')
   }
   return styles
 })
